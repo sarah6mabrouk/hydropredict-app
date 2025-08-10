85e36eb88b775138b4be1212ddccbfb1df62da00
Sarah Mabrouk <114910296+sarah6mabrouk@users.noreply.github.com>
2025-08-10 15:47:28 +0100

    tableau width - again.py

--- app.py
+++ app.py
@@ -496,27 +496,13 @@ with col2:
 # == Section 6: tableau dashboard
 st.markdown("## :blue[Visual Insights]")
 
-components.html("""
-<div class='tableauPlaceholder' id='viz168'>
-  <noscript><a href='#'><img alt='Dashboard' src='https://public.tableau.com/static/images/In/InternalPumpLeakage/Dashboard1/1_rss.png' style='border: none' /></a></noscript>
-  <object class='tableauViz' width='100%' height='900' style='display:none;'>
-    <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-    <param name='embed_code_version' value='3' />
-    <param name='site_root' value='' />
-    <param name='name' value='InternalPumpLeakage/Dashboard1' />
-    <param name='tabs' value='no' />
-    <param name='toolbar' value='yes' />
-    <param name='static_image' value='https://public.tableau.com/static/images/In/InternalPumpLeakage/Dashboard1/1.png' />
-    <param name='animate_transition' value='yes' />
-    <param name='display_static_image' value='yes' />
-    <param name='display_spinner' value='yes' />
-    <param name='display_overlay' value='yes' />
-    <param name='display_count' value='yes' />
-    <param name='language' value='en' />
-  </object>
-</div>
-<script type='text/javascript' src='https://public.tableau.com/javascripts/api/viz_v1.js'></script>
-""", height=900)
+st.markdown(
+    """
+    <iframe src="https://public.tableau.com/views/InternalPumpLeakage/Dashboard1?:showVizHome=no&:embed=true"
+            width="1200" height="900" style="border:none;"></iframe>
+    """,
+    unsafe_allow_html=True
+)
 
 
 
