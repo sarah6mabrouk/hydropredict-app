ea940b6762cb8d220956bc43ec03ee794c835e4e
Sarah Mabrouk <114910296+sarah6mabrouk@users.noreply.github.com>
2025-08-11 08:44:31 +0100

    navbar attempt8

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ if st.session_state.show_sidebar:
     <style>
     .sidebar-menu {
         position: fixed;
-        top: 80px;
+        top: 110px;
         left: 20px;
         width: 220px;
         background-color: #f5f7ff;
